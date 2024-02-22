533cc32c516a6f6c51e293232603baad9f241f6d
José Reis <jvpreis@gmail.com>
2024-02-22 15:42:51 +0000

    update volta main

--- src/main/java/battleship/Barge.java
+++ src/main/java/battleship/Barge.java
@@ -6,7 +6,7 @@ public class Barge extends Ship
     private static final String NAME = "Barca";
 
     /**
-     * @param bearing - barge bearin
+     * @param bearing - barge bearing volta main
      * @param pos     - upper left position of the barge
      */
     public Barge(Compass bearing, IPosition pos)
